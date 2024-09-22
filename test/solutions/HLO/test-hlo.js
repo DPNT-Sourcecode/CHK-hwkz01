8a911c6880451b62387cb4b92c49c5ef8c0f258a
sbx_user1051 <sbx_user1051@169.254.22.197>
2024-09-22 17:15:44 +0000

    Sun Sep 22 17:15:44 UTC 2024

--- test/solutions/HLO/test-hlo.js
+++ test/solutions/HLO/test-hlo.js
@@ -8,12 +8,12 @@ const hello = require('../../../lib/solutions/HLO/hello');
 describe('Greet tests', function() {
 	it('Return should be World with no name', ()=> {
 	    const result = hello()
-        assert.strictEqual(result,"Hello, World")
+        assert.strictEqual(result,"Hello, World!")
 	});
 
 
     it('Return should be name with no name', ()=> {
 	    const result = hello("Ajay")
-        assert.strictEqual(result,"Hello, Ajay")
+        assert.strictEqual(result,"Hello, Ajay!")
 	});
 });
