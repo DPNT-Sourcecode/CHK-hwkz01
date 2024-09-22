c005a61eadf6a2fcfc205c0c82c2d0c02af6db85
sbx_user1051 <sbx_user1051@169.254.22.197>
2024-09-22 17:11:58 +0000

    Sun Sep 22 17:11:58 UTC 2024

---
+++ test/solutions/HLO/test-hlo.js
@@ -0,0 +1,13 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const hello = require('../../../lib/solutions/HLO/hello');
+
+
+describe('Greet tests', function() {
+	it('Return should be World with no name', ()=> {
+	    const result = hello()
+        assert.strictEqual(result,"Hello, World")
+	});
+});
